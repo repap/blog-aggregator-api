cba55f16d4d08db9eb8b4f66f11e07c10a960990
Dennis Penning <penning.dennis@googlemail.com>
2019-10-29 22:08:02 +0100

    Add additional Test url

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const PORT = process.env.PORT || 3333
 
 app.get('/', (req, res) => res.send('MOIN MOIN! :)'))
 app.get('/test', (req, res) => res.send('MOIN MOIN! Das ist ein Test! :)'))
+app.get('/whup', (req, res) => res.send('WHUP, WHUP! :D :)'))
 
 app.listen(PORT, err => err ? console.error(err) : console.log('app is running') )
