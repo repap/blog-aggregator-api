ef302ded0ccd9968a6cca7b0b0dd1d68f1b35ec9
Dennis Penning <penning.dennis@googlemail.com>
2019-10-29 21:15:15 +0100

    Initial Setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3333
+
+app.get('/', (req, res) => res.send('MOIN MOIN! :)'))
+
+app.listen(PORT, err => err ? console.error(err) : `app is running` )
